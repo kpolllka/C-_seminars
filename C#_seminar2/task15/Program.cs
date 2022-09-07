f78444e8036869501e4641451bcae29c5e3fc1fe
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-07 23:25:06 +0700

    Вывод - является ли день недели выходным днем

---
+++ C#_seminar2/task15/Program.cs
@@ -0,0 +1,42 @@
+﻿// Напишите программу, которая принимает на вход цифру,
+// обозначающую день недели, и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number == 1)
+{
+     Console.Write("Понедельник - не выходной день");
+}
+else if (number == 2)
+{
+     Console.Write("Вторник - не выходной день");
+}
+else if (number == 3)
+{
+     Console.Write("Среда - не выходной день");
+}
+else if (number == 4)
+{
+     Console.Write("Четверг - не выходной день");
+}
+else if (number == 5)
+{
+     Console.Write("Пятница - не выходной день");
+}
+else if (number == 6)
+{
+     Console.Write("Суббота - выходной день");
+}
+else if (number == 7)
+{
+     Console.Write("Воскресенье - выходной день");
+}
+else
+{
+    Console.Write("Нет такого дня недели");
+}
