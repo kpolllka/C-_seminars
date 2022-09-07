15ae02f363dd05be570df162f2d10957ac760e60
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-07 22:54:51 +0700

    Вывод второго числа трехзначных чисел

---
+++ C#_seminar2/task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число
+// и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (100 <= number && number <= 999)
+{
+    int count = number % 100;
+    int count2 = count / 10;
+    Console.WriteLine(count2);
+}
+else
+Console.WriteLine("Введено не трехзначное число!");
