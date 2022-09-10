b2daceefc14668778c6b71d5f05ca7eb75d590d1
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-10 14:57:33 +0700

    Поиск расстояния между 2мя точками в 2D пространстве

--- C#_seminar3/task20/Program.cs
+++ C#_seminar3/task20/Program.cs
@@ -3,3 +3,17 @@
 // A (3,6); B (2,1) -> 5,09
 // A (7,-5); B (1,-1) -> 7,21
 
+// AB = √ (Ax – Bx)^2 + (Ay – By)^2
+
+Console.Write("Введите координату X точки A: ");
+int Ax = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y точки A: ");
+int Ay = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату X точки B: ");
+int Bx = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите координату Y точки B: ");
+int By = Convert.ToInt32(Console.ReadLine());
+
+double AB = Math.Sqrt(Math.Pow(Ax-Bx,2) + Math.Pow(Ay-By,2)); // извлечение корня квадратного
+// (внутри скобок возведение в квадрат (можно в куб и т.д.))
+Console.WriteLine($"{AB:f2}"); //f2 округлили результат до 2 знаков после запятой
