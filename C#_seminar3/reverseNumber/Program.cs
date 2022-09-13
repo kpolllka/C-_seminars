c9e79f51c0f9028f52298c3dfe0f683c5ffdd24a
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-13 22:12:06 +0700

    Разворот числа

---
+++ C#_seminar3/reverseNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿// разворот числа
+
+int number = 12345; // исходное число
+int tempNumber = number; //число, которое мы будем делить на 10
+int reversedNumber = 0; //развернутое число
+
+while(tempNumber > 0)
+{
+    int digit = tempNumber % 10;
+    reversedNumber = reversedNumber * 10 + digit;
+    tempNumber = tempNumber / 10;
+}
+Console.WriteLine($"Развернутое число равно {reversedNumber}");
