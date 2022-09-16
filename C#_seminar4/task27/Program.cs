36ee6f5c855e751976d7981c33848ce30b36317d
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-16 16:01:33 +0700

    Вывод массива из рандомных чисел

---
+++ C#_seminar4/task27/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход число
+// и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (Math.Abs(a) < 10)
+Console.WriteLine($"Сумма цифр в числе равна {Math.Abs(a)}");
+
+else
+{
+    int summ = 0;
+    int i = 0;
+    while (a > 9)
+    {
+        a = a % 10;
+        summ = summ + a;
+        i++;
+        
+    }
+    Console.WriteLine(sum);
+}
---
+++ C#_seminar4/task29/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов
+// и выводит их на экран.
+// Данные вводятся с консоли пользователем.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int a1 = ReadNumber("Введите число: ");
+int a2 = ReadNumber("Введите число: ");
+int a3 = ReadNumber("Введите число: ");
+int a4 = ReadNumber("Введите число: ");
+int a5 = ReadNumber("Введите число: ");
+int a6 = ReadNumber("Введите число: ");
+int a7 = ReadNumber("Введите число: ");
+int a8 = ReadNumber("Введите число: ");
+
+int[] array = {a1, a2, a3, a4, a5, a6, a7, a8};
+Console.WriteLine(string.Join(", ", array));
