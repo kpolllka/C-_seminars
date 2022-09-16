ebc807a9fde2a42dac0b11a73c42be65f10cd69f
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-16 13:43:37 +0700

    Вывод массива из рандомных чисел

--- C#_seminar4/task30/Program.cs
+++ C#_seminar4/task30/Program.cs
@@ -1,1 +1,12 @@
-﻿
+﻿int[] array = new int [8]; // создали массив на 8 элементов
+Random rand = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = rand.Next(0, 2);
+}
+// for (int i = 0; i < array.Length; i++)
+// {
+//     Console.Write(array[i] + " "); 
+// }
+Console.WriteLine(string.Join(", ", array));
