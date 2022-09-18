ef4f30c7694df5c9eb2e9dc6dcbdae2a14bed20a
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-18 23:38:11 +0700

    Создание копии массива поэлементно

---
+++ C#_seminar6/task45/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая будет создавать копию
+// заданного массива с помощью поэлементного копирования.
+
+int[] GetArray(int size, int leftRange,int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+
+int[] arr = GetArray(5, 0, 10);
+int[] copiedArray = new int[arr.Length];
+for(int i = 0; i < arr.Length; i++)
+{
+    copiedArray[i] = arr[i];
+}
+arr[3] = -100; // заменили значение индекса, в скопированном массиве осталось старое значение
+
+Console.WriteLine($"Исходный массив = {string.Join(", ", arr)}");
+Console.WriteLine($"Скопированный массив = {string.Join(", ", copiedArray)}");
