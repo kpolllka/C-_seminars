34638017cd7c9fdbcedc70928a4efc857a6565a9
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-18 13:47:26 +0700

    Поиск разницы между max и min элементами массива

---
+++ C#_seminar5/task38/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задайте массив натуральных чисел.
+// Найдите разницу между максимальным и минимальным элементом массива.
+// [3 7 22 2 78] -> 76
+
+int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int FindMax(int[] arr)
+{
+    int max = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(max < arr[i])
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+
+int FindMin(int[] arr)
+{
+    int min = arr[0];
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if(min > arr[i])
+        {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+
+int[] array = GetArray(10, 1, 100);
+Console.WriteLine(String.Join(", ", array));
+int max = FindMax(array);
+int min = FindMin(array);
+Console.WriteLine(max-min);
