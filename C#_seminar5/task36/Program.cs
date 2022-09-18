cfdf4410f0fcabe3167c808dc72093aaf6f6809a
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-18 13:25:54 +0700

    Поиск суммы элементов, стоящих на нечётных позициях

--- C#_seminar5/task36/Program.cs
+++ C#_seminar5/task36/Program.cs
@@ -3,3 +3,28 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int[] GetArray(int size, int leftRange, int rightRange)
+{
+    int[] arr = new int[size];
+    Random rand = new Random();
+    for(int i = 0; i < size; i++)
+    {
+        arr[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return arr;
+}
+
+int SumOfOddElements(int[] arr)
+{
+    int sum = 0;
+    for (int i = 1; i < arr.Length; i+=2)
+    {
+        sum = sum + arr[i];
+    }
+    return sum;
+} 
+
+int[] array = GetArray(6, 0, 100);
+Console.WriteLine(String.Join(", ", array));
+int sumOfOddElements = SumOfOddElements(array);
+Console.WriteLine(sumOfOddElements);
