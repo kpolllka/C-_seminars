f31694ef1b631304881f6245cd8a4af27d10b642
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-18 20:00:44 +0700

    Преобразование десятичного числа в двоичное

---
+++ C#_seminar6/task42/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintBinaryView(int number)
+{
+    if(number <= 0)
+    {
+        return; //void ничего не выводит, поэтому return исп. как окончание метода
+    }
+    PrintBinaryView(number / 2);
+    Console.Write(number % 2);
+}
+
+int num = ReadNumber("Введите число: ");
+PrintBinaryView(num);
