f7d2f6b029d1df5183a43f1587287b0fd8478f38
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-24 22:00:09 +0700

    Подсчет количества положительных чисел введенных пользователем с консоли

---
+++ C#_seminar6/task41/Program.cs
@@ -0,0 +1,30 @@
+﻿// Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// Ввод чисел останавливается при помощи ввода стоп-слова "stop"
+
+// 0, 7, 8, -2, -2 -> 2
+// 1, -7, 567, 89, 223-> 3
+
+string Message (string message)
+{
+Console.Write(message);
+return Console.ReadLine();
+}
+
+string N = Message ("Введите чило, для прекращения ввода введите stop: ");
+
+int count = 0;
+while (N != "stop")
+{
+    int i = Convert.ToInt32(N);
+    if (i > 0)
+    {
+        count = count + 1;
+        N = Message("Введите чило, для прекращения ввода введите stop: ");
+    }
+    else
+    {
+        N = Message("Введите чило, для прекращения ввода введите stop: ");
+    }
+}
+Console.WriteLine($"Вы ввели {count} чисел больше 0");
