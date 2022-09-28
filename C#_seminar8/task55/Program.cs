3087334dfb26112b098465a0916bf03cb79320e1
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-28 11:55:55 +0700

    Разворот двумерного массива

---
+++ C#_seminar8/task55/Program.cs
@@ -0,0 +1,57 @@
+﻿// Задайте двумерный массив. Напишите программу,
+// которая заменяет строки на столбцы.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnCounts, int leftRange = -10, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnCounts];
+
+    Random rand = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] NewMatrix(int[,] matrix)
+{
+    int[,] newMatrix = new int[matrix.GetLength(1), matrix.GetLength(0)];
+    for(int i = 0; i < matrix.GetLength(1); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(0); j++)
+        {
+            newMatrix[i, j] = matrix[j, i];
+        }
+    }
+    return newMatrix;
+}
+
+int m = ReadNumber("Введите количество строк: ");
+int n = ReadNumber("Введите количество столбцов: ");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+Console.WriteLine();
+int[,] result = NewMatrix(matr);
+PrintMatrix(result);
