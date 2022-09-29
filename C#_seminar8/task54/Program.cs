57abf58ebbe94e710324f47b71a55db3fb245fcf
Антонова Анастасия <sabrina-19@mail.ru>
2022-09-30 01:06:10 +0700

    Сортировка строк двумерного массива по возрастанию

---
+++ C#_seminar8/task54/Program.cs
@@ -0,0 +1,74 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит 
+// по возрастанию элементы каждой строки двумерного массива.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+
+// В итоге получается вот такой массив:
+// 1 2 4 7
+// 2 3 5 9
+// 2 4 4 8
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnCounts, int leftRange = 1, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnCounts];
+
+    Random rand = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+void NewMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+           for(int t = 0; t < matrix.GetLength(1)-1; t++)
+           {
+            if (matrix[i, t] > matrix[i, t + 1])
+            {
+                int temp = matrix [i, t];
+                matrix[i, t] = matrix[i, t + 1];
+                matrix[i, t + 1] = temp;
+            }
+           }
+        }
+        
+    }
+}
+
+int m = ReadNumber("Введите количество строк: ");
+int n = ReadNumber("Введите количество столбцов: ");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+Console.WriteLine();
+NewMatrix(matr);
+PrintMatrix(matr);
