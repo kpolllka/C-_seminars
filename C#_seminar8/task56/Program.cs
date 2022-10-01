0d1193056e1107a25da45648ca93bb6f50082c90
Антонова Анастасия <sabrina-19@mail.ru>
2022-10-02 01:03:01 +0700

    Поиск строки массива с минимальной суммой элементов

--- C#_seminar8/task56/Program.cs
+++ C#_seminar8/task56/Program.cs
@@ -1,1 +1,78 @@
-﻿
+﻿// Задайте прямоугольный двумерный массив.
+// Напишите программу, которая будет находить строку
+// с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+
+// Программа считает сумму элементов в каждой строке
+// и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnCounts, int leftRange = 1, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnCounts];
+
+    Random rand = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+
+void FindMinRow(int[,] matrix)
+{
+    int[] matrixRowsAverage = new int[matrix.GetLength(0)];
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        int sum = 0;
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            sum += matrix[i, j];
+        }
+        matrixRowsAverage[i] = sum;
+    }
+    int min = matrixRowsAverage[0];
+    int indexMin = 0;
+    for(int i = 0; i < matrixRowsAverage.Length; i++)
+    {
+        if(min > matrixRowsAverage[i])
+        {
+            indexMin = i;
+        }
+    }
+
+  Console.WriteLine($"Строка с наименьшей суммой элементов: {indexMin + 1}");
+}
+
+int m = ReadNumber("Введите количество строк: ");
+int n = ReadNumber("Введите количество столбцов: ");
+int[,] matr = GetMatrix(m, n);
+PrintMatrix(matr);
+Console.WriteLine();
+FindMinRow(matr);
