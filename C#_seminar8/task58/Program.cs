2274423ea8d8281e7dacad5339a9183dbb2dffff
Антонова Анастасия <sabrina-19@mail.ru>
2022-10-02 13:10:39 +0700

    Перемножение индексов массивов

--- C#_seminar8/task58/Program.cs
+++ C#_seminar8/task58/Program.cs
@@ -15,3 +15,63 @@
 // 20 81 8 6
 // 56 8 4 24
 // 10 6 24 49
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[,] GetMatrix(int rowsCount, int columnCounts, int leftRange = 1, int rightRange = 10)
+{
+    int[,] matrix = new int[rowsCount, columnCounts];
+
+    Random rand = new Random();
+
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i,j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for(int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for(int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i,j]}\t");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] MultMatrix(int[,] matrix1, int[,] matrix2)
+{
+    int[,] matrix3 = new int[matrix1.GetLength(0), matrix2.GetLength(1)];
+    if (matrix1.GetLength(0) == matrix2.GetLength(0) && matrix1.GetLength(1) == matrix2.GetLength(1))
+    {
+        for (int i = 0; i < matrix3.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix3.GetLength(1); j++)
+            {
+                matrix3[i, j] = matrix1[i, j] * matrix2[i, j];
+            }
+        }
+    }
+    return matrix3;
+}
+
+int m = ReadNumber("Введите количество строк: ");
+int n = ReadNumber("Введите количество столбцов: ");
+int[,] matr1 = GetMatrix(m, n);
+PrintMatrix(matr1);
+Console.WriteLine();
+int[,] matr2 = GetMatrix(m, n);
+PrintMatrix(matr2);
+Console.WriteLine();
+PrintMatrix(MultMatrix(matr1, matr2));
