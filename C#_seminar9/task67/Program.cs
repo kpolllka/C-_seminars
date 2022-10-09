030b3e6ec4d286d59e8eeeaa051c43daf2c544e1
Антонова Анастасия <sabrina-19@mail.ru>
2022-10-09 21:38:42 +0700

    Рекурсия. Вывод суммы элементов числа

---
+++ C#_seminar9/task67/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SummNum(int m)
+{
+    if (m == 0)
+    {
+        return m;
+    }
+    else
+    {
+        // int sum = m % 10 + SummNum(m / 10); // классический вариант записи
+        // return sum;
+        return m % 10 + SummNum(m / 10);
+    }
+}
+
+Console.WriteLine (SummNum(ReadNumber("Введите число M: ")));
