cd4f728ec4159f57e5d89726dd35ef8bb72201ca
Антонова Анастасия <sabrina-19@mail.ru>
2022-10-09 19:59:04 +0700

    Рекурсия. Вывод значений от M до N

--- C#_seminar9/task65/Program.cs
+++ C#_seminar9/task65/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿// Задайте значения M и N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от M до N.
+// M = 1; N = 5 -> "1, 2, 3, 4, 5"
+// M = 4; N = 8 -> "4, 5, 6, 7, 8"
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMtoN(int m, int n)
+{
+    if(m > n || m <=0)
+    {
+        return;
+    }
+    else
+    {
+        PrintMtoN(m, n-1);
+        Console.Write(n + " ");
+    }
+}
+
+int num1 = ReadNumber("Введите число M: ");
+int num2 = ReadNumber("Введите число N: ");
+PrintMtoN(num1, num2);
